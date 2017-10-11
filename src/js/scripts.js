707f84f096e6c0ed8db584e76aba20ffbd7eecfe
Must3r <must3r@gmail.com>
2017-10-11 14:27:48 +0300

    done with the responsive design

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,27 @@
+jQuery(document).ready(function() {
+  $('.popover-trigger').webuiPopover({
+    placement: 'bottom',
+    animation: 'pop'
+  });
+
+  $('#miniRegCall').on('click', function() {
+    $('.menu-mini-reg').slideToggle(300);
+  })
+
+  $('#miniListCall').on('click', function() {
+    $('.menu-mini-list').slideToggle(300);
+  })
+
+  $('#miniListCallFooter').on('click', function() {
+    $('.menu-mini-list-footer').slideToggle(300);
+  })
+
+  $('.menu-mini-close').on('click', function() {
+    $(this).parent().slideToggle(300);
+  })
+
+  $('.menu-mini-close-footer').on('click', function() {
+    $(this).parent().slideToggle(300);
+  })
+})
 // console.log("Hello, World!");
-$('.popover-trigger').webuiPopover({
-  placement: 'bottom',
-  animation: 'pop'
-});
